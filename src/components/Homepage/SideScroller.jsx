bf110a24498a797217b2c558cb25413e8104b4a4
KarthikSundar2002 <karthiksundar30092002@gmail.com>
2022-03-30 21:40:02 +0530

    feat: Integrates cycle to homePage

--- src/components/Homepage/SideScroller.jsx
+++ src/components/Homepage/SideScroller.jsx
@@ -21,10 +21,22 @@ function SideScroller() {
     const maxScroll = document.documentElement.scrollHeight
       - document.documentElement.clientHeight
       - 3;
+
     if (window.scrollY > maxScroll) {
       wrapper.scrollBy(0, deltaY);
+      document.getElementsByClassName(`${styles.cycle}`)[0].style.transform += `translateX(${deltaY * 1.95}px)`;
     }
   };
+
+  // const cycleMove = () => {
+  //   document.getElementsByClassName(`$
+  // {styles.cycle}`)[0].style.transform = `translateY(${window.scrollY}px)`;
+  // };
+  // window.onscroll = ({ deltaY }) => {
+  //   const maxScroll = document.documentElement.scrollHeight
+  //     - document.documentElement.clientHeight
+  //     - 3;
+  // };
 
   useEffect(() => {
     axios.get(`${BACKEND_URL}/events/`).then((res) => {
@@ -45,102 +57,106 @@ function SideScroller() {
   }, []);
 
   return (
-    <div
-      id="wrapper"
-      className={styles['outer-wrapper']}
-      onWheel={handleScroll}
-    >
-      <div className={styles.wrapper}>
-        <img src={cycle} alt="cycle" className={styles.cycle} />
-        <div className={`${styles.slide} ${styles.one}`}>
-          <div className={styles['content-wrapper']}>
-            <div className={styles['area-left']}>
-              <div className={styles['header-container']}>
-                <h1 className={styles['the-big-show']}>The Big Show</h1>
-                <p className={styles['sub-header']}>NITTFEST</p>
-              </div>
-            </div>
-            <div className={styles['area-right']}>
-              <p className={styles['header-about']}>
-                NITT has always believed in the philosophy that the development
-                of an individual is not accomplished through pedantic learning
-                alone but should be complimented by a healthy dose of
-                co-curricular and extra curricular activities too. The foremost
-                example of this philosophy is &apos;NITTFEST&apos; - an annual
-                extravaganza of fine arts!
-                {' '}
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className={`${styles.slide} ${styles.two}`}>
-          <img src={city} alt="city" className={styles.city} />
-          <img src={mountains} alt="mountains" className={styles.mountains} />
-          <img src={river} alt="river" className={styles.river} />
-        </div>
-        <div className={`${styles.slide} ${styles.three}`}>
-          <div className={styles['event-wrapper']}>
-            <div className={styles['event-header']}>
-              <h1>Events</h1>
-              <p>
-                It is thus evident that NITTFEST is more than just a fest - it
-                is in effect, a tribute to the creative genius inherent in every
-                Nittian. In keeping with its tradition, the high standards of
-                NITTFEST assure the sponsor a high degree of exposure, one that
-                spans a community of over 3000.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className={`${styles.slide} ${styles.four}`}>
-          {randomEvents
-            && randomEvents.map((event) => (
-              <div className={styles['event-card']} key={String(Math.random())}>
-                <div className={styles['event-content-container']}>
-                  <div className={styles['event-card-header']}>
-                    {event.name}
-                  </div>
-                  <div className={styles['event-card-content']}>
-                    {event.description}
-                  </div>
+    <>
+      {/* <img src={cycle} alt="cycle" className={styles.cycle} /> */}
+      <div
+        id="wrapper"
+        className={styles['outer-wrapper']}
+        onWheel={handleScroll}
+      >
+        <div className={styles.wrapper}>
+          <img src={cycle} alt="cycle" className={styles.cycle} />
+
+          <div className={`${styles.slide} ${styles.one}`}>
+            <div className={styles['content-wrapper']}>
+              <div className={styles['area-left']}>
+                <div className={styles['header-container']}>
+                  <h1 className={styles['the-big-show']}>The Big Show</h1>
+                  <p className={styles['sub-header']}>NITTFEST</p>
                 </div>
               </div>
-            ))}
-        </div>
-        <div className={`${styles.slide} ${styles.five}`}>
-          <img src={forest00} alt="forest" className={styles.forest} />
-          <img src={forest0} alt="forest" className={styles.forest} />
-          <img src={forest1} alt="forest" className={styles.forest} />
-        </div>
-        <div className={`${styles.slide} ${styles.six}`}>
-          <div className={styles['event-wrapper']}>
-            <div className={styles['event-header']}>
-              <h1 className={styles['about-header']}>About US</h1>
-              <p className={styles['about-content']}>
-                NITT has always believed in the philosophy that the development
-                of an individual is not accomplished through pedantic learning
-                alone but should be complimented by a healthy dose of
-                co-curricular and extra curricular activities too. The foremost
-                example of this philosophy is &apos;NITTFEST&apos; - an annual
-                extravaganza of fine arts!
-              </p>
+              <div className={styles['area-right']}>
+                <p className={styles['header-about']}>
+                  NITT has always believed in the philosophy that the development
+                  of an individual is not accomplished through pedantic learning
+                  alone but should be complimented by a healthy dose of
+                  co-curricular and extra curricular activities too. The foremost
+                  example of this philosophy is &apos;NITTFEST&apos; - an annual
+                  extravaganza of fine arts!
+                  {' '}
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-        <div className={`${styles.slide} ${styles.seven}`}>
-          <img src={rainbowbg} alt="rainbowbg" className={styles.rainbowbg} />
-          <img src={road} alt="road" className={styles.road} />
-          <img src={foliage} alt="foliage" className={styles.foliage} />
-        </div>
-        <div className={`${styles.slide} ${styles.eight}`}>
-          <p className={styles.outroHeader1}>
-            Dreaming of a better future for all?
-          </p>
-          <p className={styles.outroHeader2}>So are we.</p>
-          <div className={styles['follow-us-button']}>Follow Us</div>
+          <div className={`${styles.slide} ${styles.two}`}>
+            <img src={city} alt="city" className={styles.city} />
+            <img src={mountains} alt="mountains" className={styles.mountains} />
+            <img src={river} alt="river" className={styles.river} />
+          </div>
+          <div className={`${styles.slide} ${styles.three}`}>
+            <div className={styles['event-wrapper']}>
+              <div className={styles['event-header']}>
+                <h1>Events</h1>
+                <p>
+                  It is thus evident that NITTFEST is more than just a fest - it
+                  is in effect, a tribute to the creative genius inherent in every
+                  Nittian. In keeping with its tradition, the high standards of
+                  NITTFEST assure the sponsor a high degree of exposure, one that
+                  spans a community of over 3000.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className={`${styles.slide} ${styles.four}`}>
+            {randomEvents
+              && randomEvents.map((event) => (
+                <div className={styles['event-card']} key={String(Math.random())}>
+                  <div className={styles['event-content-container']}>
+                    <div className={styles['event-card-header']}>
+                      {event.name}
+                    </div>
+                    <div className={styles['event-card-content']}>
+                      {event.description}
+                    </div>
+                  </div>
+                </div>
+              ))}
+          </div>
+          <div className={`${styles.slide} ${styles.five}`}>
+            <img src={forest00} alt="forest" className={styles.city} />
+            <img src={forest1} alt="forest" className={styles.mountains} />
+            <img src={forest0} alt="forest" className={styles.river} />
+          </div>
+          <div className={`${styles.slide} ${styles.six}`}>
+            <div className={styles['event-wrapper']}>
+              <div className={styles['event-header']}>
+                <h1 className={styles['about-header']}>About US</h1>
+                <p className={styles['about-content']}>
+                  NITT has always believed in the philosophy that the development
+                  of an individual is not accomplished through pedantic learning
+                  alone but should be complimented by a healthy dose of
+                  co-curricular and extra curricular activities too. The foremost
+                  example of this philosophy is &apos;NITTFEST&apos; - an annual
+                  extravaganza of fine arts!
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className={`${styles.slide} ${styles.seven}`}>
+            <img src={rainbowbg} alt="rainbowbg" className={styles.city} />
+            <img src={road} alt="road" className={styles.mountains} />
+            <img src={foliage} alt="foliage" className={styles.river} />
+          </div>
+          <div className={`${styles.slide} ${styles.eight}`}>
+            <p className={styles.outroHeader1}>
+              Dreaming of a better future for all?
+            </p>
+            <p className={styles.outroHeader2}>So are we.</p>
+            <div className={styles['follow-us-button']}>Follow Us</div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
